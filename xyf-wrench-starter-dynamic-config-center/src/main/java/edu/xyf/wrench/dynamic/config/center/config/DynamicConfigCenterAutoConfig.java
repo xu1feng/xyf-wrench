579d63d9fa4e5680f2c4b138a3893b67157f7bb8
2465549609@qq.com <xuyifeng@xuyifengdeMacBook-Air.local>
2025-10-17 15:02:45 +0800

    feat: init project

---
+++ xyf-wrench-starter-dynamic-config-center/src/main/java/edu/xyf/wrench/dynamic/config/center/config/DynamicConfigCenterAutoConfig.java
@@ -0,0 +1,15 @@
+package edu.xyf.wrench.dynamic.config.center.config;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+/**
+ * @Author: Xuyifeng
+ * @Description:
+ * @Date: 2025/10/17 15:01
+ */
+
+public class DynamicConfigCenterAutoConfig implements BeanPostProcessor {
+
+
+
+}
